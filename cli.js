9fad2a6c58bef8d38720e9ee9dae5aec7a1a6a4a
zhangHaha <zhaha_1122@163.coom>
2022-06-26 01:01:01 +0800

    '用户命令处理'

--- cli.js
+++ cli.js
@@ -1,29 +1,55 @@
 #! /usr/bin/env node
-const inquirer = require('inquirer');
-const path = require('path');
-const fs = require('fs');
-const ejs = require('ejs');
 
-inquirer.prompt([
-  {
-    type: 'input', //type： input, number, confirm, list, checkbox ... 
-    name: 'name', // key 名
-    message: 'Your name', // 提示信息
-    default: 'my-node-cli' // 默认值
-  }
-]).then(answers => {
-  // 打印互用输入结果
-  const destUrl = path.join(__dirname, 'templates');
+const program = require('commander');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
-  const cwdUrl = process.cwd();
+const projectName = require('./package.json').name;
 
-  fs.readdir(destUrl, (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-      ejs.renderFile(path.join(destUrl, file), answers).then(data => {
-        fs.writeFileSync(path.join(cwdUrl, file), data);
-      })
-    });
+
+program
+  .command('create <app-name>')
+  .description('create a new project')
+  .option('-f, --force', 'overwrite target directory if it exist')
+  .action((name, options) => {
+    require('./lib/create.js')(name, options);
   })
-  console.log(answers)
-})
+
+// 配置config命令
+program
+.command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    require('./lib/config.js')(value, options);
+  })
+
+// 配置ui命令
+program
+  .command('ui')
+  .description('start add open aa-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    require('./lib/ui.js')(option)
+  })
+
+program
+  .on('--help', () => {
+
+    console.log('\r\n' + figlet.textSync('AA-CLI', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }))
+    console.log(`\r\n ${chalk.blue(`${projectName} <command> --help`)} for detailed usage of given command\r\n`)
+  })
+
+program
+  .version(`v${require('./package.json').version}`)
+  .usage('<command> [option]')
+
+program.parse(process.argv);
